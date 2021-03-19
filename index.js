39f49145299296e026366dbee0acf28dee31f220
meow <you@example.com>
2021-03-19 05:40:34 -0700

    more_conventions

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ let name= 'david';
 console.log(name);
 //cannot be a reserved keyword for naming variables
 // should be meaningful names 
+// cannot start with a number and or spaces
+// variable names are case sensitive 
+//conventionally you capatilize the new words like the example below
+let firstName;
