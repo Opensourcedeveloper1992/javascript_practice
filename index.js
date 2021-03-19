85a89c5e1075963c4e858ed6aaaff7d9f7266b8b
meow <you@example.com>
2021-03-19 05:49:37 -0700

    const_error_intentional

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ console.log(name);
 // cannot start with a number and or spaces
 // variable names are case sensitive 
 //conventionally you capatilize the new words like the example below
-let firstName;
+//nospaces or hypens in variables
+// convention in Javascript below
+let firstName="david";
+let lastName ="insert_last_name_here";
+
+
+
+//not conventional
+let FirstName;
+
+
---
+++ more.js
@@ -0,0 +1,5 @@
+//const error should occur
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
