aef15b04c9898b6c9389876fa1f9c17e80879017
meow <you@example.com>
2021-03-19 05:37:24 -0700

    console_logging_commenting_basics

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
   // this is my first java script comment
-  console.log('helloworld');
+//  console.log('helloworld');
+let name= 'david';
+console.log(name);
+//cannot be a reserved keyword for naming variables
+// should be meaningful names 
