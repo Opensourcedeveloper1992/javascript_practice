8366bfc792ef5a679dd19ebca8374f127fa4d04b
meow <you@example.com>
2021-03-21 07:33:47 -0700

    primitive_types_dynamic_typing

---
+++ primitivetypes.js
@@ -0,0 +1,16 @@
+// string,number boolean, undefinded all are primtive types
+
+//string literal
+let name ="david";
+let age = 28; // number literal
+let isApproved= true; //Boolean literal true and false revered keywords
+let firstName = undefined; 
+let lastName= null; //will clear the value of the variable
+let selectedColor = null;
+//es6 symbol covered later
+// javascript is dyanamically typed
+
+console.log(typeof name); 
+
+name =1; 
+console.log(typeof name);
