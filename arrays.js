b2af795cddfcf5a82bdb07c61d080b8950f784f6
meow <you@example.com>
2021-03-21 08:00:38 -0700

    functions

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
 let selcetedColors = ['red', 'blue']; //array literal empty 
 selcetedColors[2]= 'green';
 console.log(selcetedColors[2]);
+console.log(selcetedColors.length);
+
+//arays are objects they have key value pairs
+
+//array is data structure used to represnet a list of items
+
