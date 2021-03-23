17ff2442bc9dab47ae501d80ffb5927c63cbf77a
meow <you@example.com>
2021-03-23 06:29:15 -0700

    function_calculates_square

--- functions.js
+++ functions.js
@@ -3,7 +3,14 @@ function greet(name, lastName){
         lastName='no last name input';
      }
     console.log('Hello ' + name + " " +lastName);
-
+// template literals later to help clean up code
+}
+// caluclating a value
+function square(number){
+    return number * number;
 }
 // name is parameter of greet
-greet('John', 'ping'); //john is argumennt for greet
+//greet('John', 'ping'); //john is argumennt for greet
+let number = square(2);
+console.log(number);
+
